01ad75cc1eda081ed17793236505af9694977432
birdiesama <46026849+birdiesama@users.noreply.github.com>
2021-02-17 18:06:15 -0500

    Create rfx_bshEn2.py

---
+++ rfx_bshEn2.py
@@ -0,0 +1,20 @@
+import pymel.core as pm
+
+value = 0.25
+
+selection_list = pm.ls(sl = True)
+
+crv_shape_list = pm.listRelatives(selection_list, type = 'nurbsCurve')
+crv_shape_list = list(set(crv_shape_list))
+
+for shape in crv_shape_list:
+    if shape.isIntermediate():
+        crv_shape_list.remove(shape)
+
+bsh_list = pm.listConnections(crv_shape_list, type = 'blendShape')
+bsh_list = list(set(bsh_list))
+
+for bsh in bsh_list[0:1]:
+    if 'OUTPUT' in bsh.nodeName():
+        attrNm = bsh.stripNamespace().split('_BS')[0]
+        pm.setAttr('{0}.{1}Shape'.format(bsh.nodeName(), attrNm), 0.1)
