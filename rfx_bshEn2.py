d34fadd5a5a4ed4a8d6e664f3d2914b2019504ae
birdiesama <46026849+birdiesama@users.noreply.github.com>
2021-02-17 18:20:08 -0500

    Update rfx_bshEn2.py

--- rfx_bshEn2.py
+++ rfx_bshEn2.py
@@ -1,20 +1,22 @@
 import pymel.core as pm
 
-value = 0.25
+value = .33
 
 selection_list = pm.ls(sl = True)
 
-crv_shape_list = pm.listRelatives(selection_list, type = 'nurbsCurve')
-crv_shape_list = list(set(crv_shape_list))
+for selection in selection_list:
 
-for shape in crv_shape_list:
-    if shape.isIntermediate():
-        crv_shape_list.remove(shape)
+    crv_shape_list = pm.listRelatives(selection, type = 'nurbsCurve')
+    crv_shape_list = list(set(crv_shape_list))
 
-bsh_list = pm.listConnections(crv_shape_list, type = 'blendShape')
-bsh_list = list(set(bsh_list))
+    for shape in crv_shape_list:
+        if shape.isIntermediate():
+            crv_shape_list.remove(shape)
 
-for bsh in bsh_list[0:1]:
-    if 'OUTPUT' in bsh.nodeName():
-        attrNm = bsh.stripNamespace().split('_BS')[0]
-        pm.setAttr('{0}.{1}Shape'.format(bsh.nodeName(), attrNm), value)
+    bsh_list = pm.listConnections(crv_shape_list, type = 'blendShape')
+    bsh_list = list(set(bsh_list))
+
+    for bsh in bsh_list:
+        if 'OUTPUT' in bsh.nodeName():
+            attrNm = bsh.stripNamespace().split('_BS')[0]
+            pm.setAttr('{0}.{1}Shape'.format(bsh.nodeName(), attrNm), value)
