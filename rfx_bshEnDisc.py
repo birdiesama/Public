b1135c85a315e8c5a4aab1e17cabe48d7025172a
birdiesama <46026849+birdiesama@users.noreply.github.com>
2021-05-04 16:25:03 -0400

    Update rfx_bshEnDisc.py

--- rfx_bshEnDisc.py
+++ rfx_bshEnDisc.py
@@ -1,5 +1,32 @@
 import pymel.core as pm
 
+# select your curve
+selection_list = pm.ls(sl = True)
+
+# list all curve nodes from selection
+crv_shape_list = pm.listRelatives(selection_list, type = 'nurbsCurve')
+# remove duplicates
+crv_shape_list = list(set(crv_shape_list))
+
+# remove unused shapes (e.g. orig)
+for shape in crv_shape_list:
+    if shape.isIntermediate():
+        crv_shape_list.remove(shape)
+
+# list blendshapes from crv nodes
+#bsh_list = pm.listConnections(crv_shape_list, type = 'blendShape')
+con_list = pm.listConnections(crv_shape_list, shapes = True)
+con_list = list(set(con_list))
+
+for con in con_list:
+    if 'OUTPUT_BSSet' in con.nodeName():
+        bsn = pm.PyNode(con.nodeName().replace('_BSSet', '_BS'))
+        bsn.en.disconnect()
+
+
+'''
+import pymel.core as pm
+
 # select your curve / group
 selection_list = pm.ls(sl = True)
 
@@ -20,4 +47,4 @@ bsh_list = list(set(bsh_list))
 for bsh in bsh_list:
     if 'OUTPUT' in bsh.nodeName():
         bsh.en.disconnect()
-
+'''
