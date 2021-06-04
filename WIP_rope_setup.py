00c57d743d0e22357d7c1f42b73b9c248e648457
birdiesama <46026849+birdiesama@users.noreply.github.com>
2021-06-04 19:28:08 -0400

    Update WIP_rope_setup.py

--- WIP_rope_setup.py
+++ WIP_rope_setup.py
@@ -50,3 +50,30 @@ for i in range(int(startFrame), int(endFrame+1)):
     pos_list.append(pm.getAttr(target.t, time = i))
 
 pm.curve(d = 3, p = pos_list)
+
+////
+
+sel_pymel.core as pm
+
+sel_list = pm.ls(sl = True)
+jnt_list = sel_list
+jnt_list.extend(pm.listRelatives(sel_list, ad = True))
+jnt_list.reverse()
+
+tfm_grp = pm.group(em = True, n = 'pxy_grp')
+
+target_crv = pm.PyNode('SR_rope_anim_crv')
+
+counter = 1
+
+for jnt in jnt_list:
+    poci = pm.createNode('pointOnCurveInfo', n = jnt.nodeName() + '_poci')
+    target_crv.worldSpace >> poci.inputCurve
+    poci.turnOnPercentage.set(True)
+    par = 1.0/len(jnt_list)*counter
+    poci.parameter.set(par)
+    pxy_group = pm.group(em = True, n = jnt.nodeName() + '_pxy')
+    pm.parent(pxy_grp, tfm_grp)
+    poci.position >> pxy_group.t
+    pm.parentConstraint(pxy_group, jnt, skipRotate = 'None', skipTranslate = 'None', mo = False)
+    counter += 1
